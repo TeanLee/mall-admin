81c225acbaa2789e2ea5eb0ed821820109e46270
litingting <ltt598625763@163.com>
2021-12-28 20:58:24 +0800

    实现订单列表和分类列表页面的框架

--- mall-admin-frontend/src/views/category/list.vue
+++ mall-admin-frontend/src/views/category/list.vue
@@ -1,13 +1,65 @@
 <template>
-  <div>category</div>
+  <div class="category-list">
+    <h1>分类列表</h1>
+    <el-table
+      :data="tableData"
+      border
+      style="width: 100%">
+      <el-table-column
+        prop="id"
+        label="ID">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="分类名">
+      </el-table-column>
+      <el-table-column label="banner">
+        <template slot-scope="{row}">
+          <el-link type="primary" :href="row.banner" target="_blank">{{ row.banner }}</el-link>
+        </template>
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="200">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'category-list',
+  data() {
+    return {
+      tableData: [
+        {
+          id: '1',
+          name: '11',
+          banner: '111'
+        },
+        {
+          id: '2',
+          name: '22',
+          banner: '222'
+        }
+      ],
+    }
+  },
+  created() {
+    
+  },
+  methods: {
+    
+  }
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+.category-list {
+  padding: 0 20px;
+}
 </style>
