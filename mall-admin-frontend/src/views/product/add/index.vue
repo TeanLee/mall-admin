a5de1ca5f29196a51fe0933ecbd6054d195e7428
litingting <ltt598625763@163.com>
2021-12-29 09:28:20 +0800

    实现添加商品的表单

--- mall-admin-frontend/src/views/product/add/index.vue
+++ mall-admin-frontend/src/views/product/add/index.vue
@@ -1,13 +1,95 @@
 <template>
-  <div>add product</div>
+  <div class="product-add">
+    <h1>上架商品</h1>
+    <div class="add-form">
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="商品名" prop="title">
+          <el-input v-model="ruleForm.title"></el-input>
+        </el-form-item>
+        <el-form-item label="商品描述" prop="subTitle">
+          <el-input v-model="ruleForm.subTitle"></el-input>
+        </el-form-item>
+        <el-form-item label="商品价格" prop="price">
+          <el-input v-model="ruleForm.price"></el-input>
+        </el-form-item>
+        <el-form-item label="商品单位" prop="unit">
+          <el-input v-model="ruleForm.banner"></el-input>
+        </el-form-item>
+        <el-form-item label="商品主图" prop="banner">
+          <el-input v-model="ruleForm.banner"></el-input>
+        </el-form-item>
+        <el-form-item label="商品分类" prop="category">
+          <el-select v-model="ruleForm.region" placeholder="请选择商品分类">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      ruleForm: {
+        title: '',
+        subTitle: '',
+        price: '',
+        unit: '',
+        banner: '',
+        category: ''
+      },
+      rules: {
+        title: [
+          { required: true, message: '请输入商品名', trigger: 'blur' },
+          // { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        price: [
+          { required: true, message: '请输入商品价格', trigger: 'change' }
+        ],
+        unit: [
+          { required: true, message: '请输入计量单位', trigger: 'blur' },
+          { max: 1, message: '长度为 1 个字符', trigger: 'blur' }
+        ],
+        banner: [
+          { required: true, message: '请输入商品主图', trigger: 'change' }
+        ],
+        category: [
+          { required: true, message: '请选择活动区域', trigger: 'change' }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.product-add {
+  padding: 0 20px;
+  .add-form {
+    width: 50%;
+    margin: 0 auto;
+  }
+}
 </style>
--- mall-admin-frontend/src/views/product/list/index.vue
+++ mall-admin-frontend/src/views/product/list/index.vue
@@ -1,15 +1,175 @@
 <template>
-  <div>
-    product list
+  <div class="product-list">
+    <h1>商品列表</h1>
+    <div class="header-search">
+      <div class="tip">
+        <div class="left">
+          <i class="el-icon-search"></i>
+          <span>筛选搜索</span>
+        </div>
+        <div class="right">
+          <el-button size="small">重置</el-button>
+          <el-button type="primary" size="small">查询结果</el-button>
+        </div>
+      </div>
+      <div class="inputs">
+        <el-row>
+          <el-col :span="10"><div class="demo-input-suffix">商品名称：<el-input v-model="productName" placeholder="请输入内容" style="display:inline"></el-input></div></el-col>
+          <el-col :span="4"><div class="grid-content bg-purple-dark"></div></el-col>
+          <el-col :span="10">
+            <div class="demo-input-suffix">商品分类：
+              <el-select v-model="value" placeholder="请选择">
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+    <el-table
+      :data="tableData"
+      border
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名">
+      </el-table-column>
+      <el-table-column
+        prop="province"
+        label="省份">
+      </el-table-column>
+      <el-table-column
+        prop="city"
+        label="市区">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+      <el-table-column
+        prop="zip"
+        label="邮编">
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="200">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" size="small">编辑</el-button>
+          <el-button type="danger" size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-export default {
+  export default {
+    methods: {
+      handleClick(row) {
+        console.log(row);
+      }
+    },
 
-}
+    data() {
+      return {
+        productName: '',
+        options: [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+        value: '',
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1517 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1519 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1516 弄',
+          zip: 200333
+        }]
+      }
+    }
+  }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.product-list {
+  padding: 0 20px;
 
+  .header-search {
+    width: 100%;
+    height: 120px;
+    border: 1px solid #EBEEF5;
+    margin-bottom: 30px;
+    padding: 15px 100px 15px 10px;
+
+    .tip {
+      width: 100%;
+      height: 30px;
+      .left {
+        float: left;
+      }
+      .right {
+        float: right;
+      }
+    }
+
+    .inputs {
+      padding-top: 20px;
+    }
+  }
+}
+.demo-input-suffix {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .el-input {
+    width: 70%;
+  }
+  .el-select {
+    width: 70%;
+  }
+}
 </style>
