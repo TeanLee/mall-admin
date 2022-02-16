4004fd6b1018ddccc6f74e0a135336937fceef87
litingting <ltt598625763@163.com>
2022-02-16 19:05:24 +0800

    将 redis 中存储的 session 信息从默认的 gob 改成 json 格式

--- mall-admin-server-go/main.go
+++ mall-admin-server-go/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/boj/redistore"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
@@ -10,11 +11,18 @@ import (
 
 func main() {
 	r := gin.Default()
-
-	store, err := redis.NewStore(10, "tcp", "localhost:6379", "", []byte(""))
+	store, err := redis.NewStore(10, "tcp", "localhost:6379", "", []byte("1"))
 	if err != nil {
 		panic(err)
 	}
+
+	// 将 redis 中存储的 session 信息从默认的 gob 改成 json 格式（便于查看，但是传输效率会下降）
+	err, reStore := redis.GetRedisStore(store)
+	if err != nil {
+		panic(err)
+	}
+	reStore.SetSerializer(redistore.JSONSerializer{})
+
 	r.Use(sessions.Sessions("mysession", store))
 
 	r.Use(middleware.Cors())
