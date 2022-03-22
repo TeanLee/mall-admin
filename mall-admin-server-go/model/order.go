20fdabbfbb52a52e95aa7d1701799d3c8f96e15d
litingting <ltt598625763@163.com>
2022-03-22 17:06:35 +0800

    get all orders

--- mall-admin-server-go/model/order.go
+++ mall-admin-server-go/model/order.go
@@ -1,14 +1,32 @@
 package model
 
-type (
-	StringInterfaceMap map[string]interface{}
-	Order              struct {
-		OrderItem string `json:"order_item"`
-		OrderTime string `json:"order_time"`
-		Uid       int    `json:"uid"` // float64 实际对应的是 double
-		Status    int    `json:"status"`
-	}
+import (
+	"database/sql/driver"
+	"encoding/json"
 )
+
+type Order struct {
+	OrderItem OrderItemType `gorm:"TYPE:json" json:"order_item"`
+	OrderTime string        `json:"order_time"`
+	Uid       int           `json:"uid"` // float64 实际对应的是 double
+	Status    int           `json:"status"`
+}
+
+type OrderItemType []struct {
+	Count     int `json:"count"`
+	ProductID int `json:"productId"`
+}
+
+// Scan 实现 sql.Scanner 接口，Scan 将 value 扫描至 OrderItemType
+func (c *OrderItemType) Scan(input interface{}) error {
+	return json.Unmarshal(input.([]byte), c)
+}
+
+// Value 实现 driver.Valuer 接口，Value 返回 OrderItemType value
+func (c OrderItemType) Value() (driver.Value, error) {
+	b, err := json.Marshal(c)
+	return string(b), err
+}
 
 func (Order) TableName() string {
 	return "order"
--- mall-admin-server-go/service/order.srv.go
+++ mall-admin-server-go/service/order.srv.go
@@ -1,14 +1,66 @@
 package service
 
-import "mall-admin-server-go/model"
+import (
+	"mall-admin-server-go/model"
+)
 
 type OrderSrv struct{}
 
-func GetOrders(offset int, pageSize int, productName string, category string) ([]model.Order, int64, error) {
-	products, err := model.GetOrders(offset, pageSize, productName, category)
+type ProductItem struct {
+	Title      string  `json:"title"`
+	Banner     string  `json:"banner"`
+	Price      float64 `json:"price"`
+	CategoryId int     `json:"category_id"`
+}
+
+type OrderItem struct {
+	Count       int         `json:"count"`
+	ProductItem ProductItem `json:"product,inline"`
+}
+
+func newOrderItem(productItem ProductItem) OrderItem {
+	return OrderItem{
+		Count:       0,
+		ProductItem: productItem,
+	}
+}
+
+type OrderResType struct {
+	OrderItems []OrderItem `json:"order_items"`
+	OrderTime  string      `json:"order_time"`
+	Uid        int         `json:"uid"`
+	Status     int         `json:"status"`
+}
+
+func GetOrders(offset int, pageSize int, productName string, category string) ([]OrderResType, int64, error) {
+	var orders []model.Order
+	orders, err := model.GetOrders(offset, pageSize, productName, category)
+
+	var resOrder []OrderResType
 	total := model.CountOrder()
+
+	// 根据订单中的 productId 查找对应的商品
+	for _, order := range orders {
+		var tempOrder OrderResType
+		for _, item := range order.OrderItem {
+			var tempItem ProductItem
+			var product model.Product
+			if product, err = model.GetProductById(item.ProductID); err != nil {
+				tempItem.Title = product.Title
+				tempItem.Banner = product.Banner
+				tempItem.Price = product.Price
+				tempItem.CategoryId = product.CategoryId
+			}
+			tempOrderItem := newOrderItem(tempItem)
+			tempOrderItem.Count = item.Count
+			tempOrder.OrderItems = append(tempOrder.OrderItems, tempOrderItem)
+			tempOrder.Uid = order.Uid
+		}
+		resOrder = append(resOrder, tempOrder)
+	}
+
 	if err != nil {
 		return nil, total, err
 	}
-	return products, total, nil
+	return resOrder, total, nil
 }
